a4ccfbce144220743d966c1abb319c16bb340c0e
Ivan Litovka <ivan.litovka@gmail.com>
2026-01-03 17:06:10 +0300

    3x3

---
+++ tic_tac_toe/game.py
@@ -0,0 +1,19 @@
+from gameparts import Board
+
+
+def main():
+    # Создать игровое поле - объект класса Board.
+    game = Board()
+    # Отрисовать поле в терминале.
+    game.display()
+    # Разместить на поле символ по указанным координатам - сделать ход.
+    game.make_move(1, 2, 'X')
+    game.make_move(0, 0, 'X')
+    game.make_move(2, 2, 'X')
+    print('Ход сделан!')
+    # Перерисовать поле с учётом сделанного хода.
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tic_tac_toe/gameparts/parts.py
@@ -0,0 +1,23 @@
+# Объявить класс.
+class Board:
+    # Инициализировать игровое поле - список списков с пробелами.
+    # Пробелы - это пустые клетки.
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+
+    # Метод, который обрабатывает ходы игроков.
+    def make_move(self, row, col, player):
+        self.board[row][col] = player
+
+    # Метод, который отрисовывает игровое поле.
+    def display(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
+
+    # Переопределяем метод __str__.
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
