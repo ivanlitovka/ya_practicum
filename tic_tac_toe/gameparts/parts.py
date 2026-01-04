ab24f2ac0106f6eddac74559bf8a0e9d4058eb40
Ivan Litovka <ivan.litovka@gmail.com>
2026-01-04 16:07:54 +0300

    up

--- tic_tac_toe/gameparts/parts.py
+++ tic_tac_toe/gameparts/parts.py
@@ -1,17 +1,18 @@
-# Объявить класс.
-class Board:
-    """
-    Класс, который описывает игровое поле.
-    Пробелы - это пустые клетки.
-    """
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+# gameparts/parts.py
 
-    # Метод, который обрабатывает ходы игроков.
+class Board:
+
+    # Новый атрибут.
+    field_size = 3
+
+    def __init__(self):
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
+
     def make_move(self, row, col, player):
         self.board[row][col] = player
 
-    # Метод, который отрисовывает игровое поле.
     def display(self):
         for row in self.board:
             print('|'.join(row))
