10e2a060c06185178bc3365c2911d147ca6ad0e8
Ivan Litovka <ivan.litovka@gmail.com>
2026-08-24 15:14:56 +0300

    PSQL done

--- DWH для нескольких источников/s5-lessons/dags/src_ranks_and_users_to_stg.py
+++ DWH для нескольких источников/s5-lessons/dags/src_ranks_and_users_to_stg.py
@@ -1,35 +1,86 @@
+"""
+DAG для загрузки данных из источника в DWH
+"""
 from airflow.decorators import dag, task
 import logging
 import pendulum
 
-# Импортируем из нашей библиотеки
-from my_lib import extract_load_table
+from my_lib import (
+    extract_load_table,
+    get_last_loaded_id,
+    save_loaded_state
+)
 
 log = logging.getLogger(__name__)
 
 
 @task(task_id='extract_load_ranks_via_hook')
 def extract_load_ranks():
-    """Загрузка таблицы ranks"""
-    return extract_load_table(
+    """Загрузка таблицы ranks (полная перезагрузка)"""
+    extract_load_table(
         source_conn_id='PG_ORIGIN_BONUS_SYSTEM_CONNECTION',
         dwh_conn_id='PG_WAREHOUSE_CONNECTION',
         source_table='ranks',
         dwh_table='bonussystem_ranks',
-        dwh_schema='stg'
+        dwh_schema='stg',
+        truncate=True
     )
 
 
 @task(task_id='extract_load_users_via_hook')
 def extract_load_users():
-    """Загрузка таблицы users"""
-    return extract_load_table(
+    """Загрузка таблицы users (полная перезагрузка)"""
+    extract_load_table(
         source_conn_id='PG_ORIGIN_BONUS_SYSTEM_CONNECTION',
         dwh_conn_id='PG_WAREHOUSE_CONNECTION',
         source_table='users',
         dwh_table='bonussystem_users',
-        dwh_schema='stg'
+        dwh_schema='stg',
+        truncate=True
     )
+
+
+@task(task_id='extract_load_events_via_hook')
+def extract_load_events():
+    """
+    Загрузка таблицы outbox (инкрементальная с сохранением состояния)
+    """
+    log.info('Начинаем загрузку outbox -> stg.bonussystem_events')
+    
+    # 1. Получаем последний загруженный id из таблицы настроек
+    last_id = get_last_loaded_id(
+        dwh_conn_id='PG_WAREHOUSE_CONNECTION',
+        workflow_key='outbox_events_load',
+        schema='stg',
+        default=0
+    )
+    log.info(f'Последний загруженный id: {last_id}')
+    
+    # 2. Загружаем данные с фильтром по id
+    df = extract_load_table(
+        source_conn_id='PG_ORIGIN_BONUS_SYSTEM_CONNECTION',
+        dwh_conn_id='PG_WAREHOUSE_CONNECTION',
+        source_table='outbox',
+        dwh_table='bonussystem_events',
+        dwh_schema='stg',
+        truncate=False,
+        query=f'SELECT * FROM outbox WHERE id > {last_id} ORDER BY id ASC'
+    )
+    
+    # 3. Если есть данные - сохраняем состояние
+    if df is not None and not df.empty:
+        new_max_id = int(df['id'].max())
+        log.info(f'Новый максимальный id: {new_max_id}')
+        
+        save_loaded_state(
+            dwh_conn_id='PG_WAREHOUSE_CONNECTION',
+            workflow_key='outbox_events_load',
+            last_loaded_id=new_max_id,
+            schema='stg'
+        )
+        log.info(f'Сохранили новый last_loaded_id: {new_max_id}')
+    else:
+        log.info('Нет новых записей для загрузки')
 
 
 @dag(
@@ -38,7 +89,6 @@ def extract_load_users():
     catchup=False,
     tags=['sprint5', '5_2', 'ex_load_table_psql'],
     is_paused_upon_creation=False,
-    doc_md=__doc__,
     default_args={
         'owner': 'airflow',
         'retries': 1,
@@ -49,10 +99,10 @@ def load_data_to_stg():
     """Загрузка данных из источника в STG слой"""
     ranks_task = extract_load_ranks()
     users_task = extract_load_users()
+    events_task = extract_load_events()
     
-    # Задачи могут выполняться параллельно
-    # Если нужно последовательно: ranks_task >> users_task
-    [ranks_task, users_task]
+    # Все задачи выполняются параллельно
+    [ranks_task, users_task, events_task]
 
 
 dag = load_data_to_stg()
